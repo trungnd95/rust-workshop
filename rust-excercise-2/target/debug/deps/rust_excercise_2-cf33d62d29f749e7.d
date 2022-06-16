455944d934b5b686dffb8bf879d8de1459644605
Trung-Ng <trungnd233@viettel.com.vn>
2022-06-16 13:54:06 +0700

    ex-2

---
+++ rust-excercise-2/target/debug/deps/rust_excercise_2-cf33d62d29f749e7.d
@@ -0,0 +1,5 @@
+/home/trung-ng/workspace/mine/rust/rust-workshop/rust-excercise-2/target/debug/deps/rust_excercise_2-cf33d62d29f749e7: src/main.rs
+
+/home/trung-ng/workspace/mine/rust/rust-workshop/rust-excercise-2/target/debug/deps/rust_excercise_2-cf33d62d29f749e7.d: src/main.rs
+
+src/main.rs:
---
+++ rust-excercise-2/target/debug/rust-excercise-2.d
@@ -0,0 +1,1 @@
+/home/trung-ng/workspace/mine/rust/rust-workshop/rust-excercise-2/target/debug/rust-excercise-2: /home/trung-ng/workspace/mine/rust/rust-workshop/rust-excercise-2/src/main.rs
