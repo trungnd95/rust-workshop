d1237dfa56bf7e304c55c2c07053a92715121a25
Trung-Ng <trungnd233@viettel.com.vn>
2022-06-24 08:37:06 +0700

    VBI rust workshop ex4

---
+++ rust-excercise-4/target/debug/deps/rust_excercise_4-7e1629894d5d866c.d
@@ -0,0 +1,5 @@
+/home/trung-ng/workspace/mine/rust/rust-workshop/rust-excercise-4/target/debug/deps/rust_excercise_4-7e1629894d5d866c: src/main.rs
+
+/home/trung-ng/workspace/mine/rust/rust-workshop/rust-excercise-4/target/debug/deps/rust_excercise_4-7e1629894d5d866c.d: src/main.rs
+
+src/main.rs:
---
+++ rust-excercise-4/target/debug/rust-excercise-4.d
@@ -0,0 +1,1 @@
+/home/trung-ng/workspace/mine/rust/rust-workshop/rust-excercise-4/target/debug/rust-excercise-4: /home/trung-ng/workspace/mine/rust/rust-workshop/rust-excercise-4/src/main.rs
